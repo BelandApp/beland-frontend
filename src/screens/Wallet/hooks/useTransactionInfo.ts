a1c50646cc29b67c96f5a5d45bb629b5a0b864da
VLDeMenezes <estudiodemenezes@gmail.com>
2025-12-30 08:01:18 -0300

    feat hook Info Transaction

---
+++ src/screens/Wallet/hooks/useTransactionInfo.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { Transaction } from "../types";
+import { CoreApiService } from "src/services";
+const _core = new CoreApiService();
+export type InfoItems = {
+  producto: string;
+  cantidad: number;
+}
+export const useTransactionInfo = (transaction: Transaction) => { 
+  const [info, setInfo] = useState<InfoItems[]|null>(null);
+  useEffect(() => {
+    const fetchTransactionInfo = async () => {
+      const ORDER_ID = transaction?.from?.split(" ")[1];
+      const res = await _core.get("orders/" + ORDER_ID);
+      setInfo(res.items.map((item: any) => ({
+        producto: item.product.name,
+        cantidad: item.quantity
+      })));
+    }
+    if(transaction.type === "pago")
+    fetchTransactionInfo();
+  },[])
+  return { info }
+}
