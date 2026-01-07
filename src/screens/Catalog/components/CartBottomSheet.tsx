c8981822aa16c9c5d6ad8b80458c9c5ae7d920b6
VLDeMenezes <estudiodemenezes@gmail.com>
2026-01-07 08:29:23 -0300

    fix error on Text child on CartBottom

--- src/screens/Catalog/components/CartBottomSheet.tsx
+++ src/screens/Catalog/components/CartBottomSheet.tsx
@@ -206,8 +206,8 @@ export const CartBottomSheet: React.FC<CartBottomSheetProps> = ({
                   }
                   onCheckout && onCheckout();
                 }}
-              />{" "}
-            </View>{" "}
+              />
+            </View>
           </View>
         }
       />
