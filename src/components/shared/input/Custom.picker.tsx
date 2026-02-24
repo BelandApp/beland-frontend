7e18da8391bc638f53bebbe14fd80ba7549fa5fa
Victor De Menezes <estudiodemenezes@gmail.com>
2026-02-23 22:31:07 -0300

    feat Custom Picker component

---
+++ src/components/shared/input/Custom.picker.tsx
@@ -0,0 +1,70 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Picker, PickerProps } from "@react-native-picker/picker";
+type PickerValue = string | null;
+
+interface CustomPickerProps extends Omit<
+  PickerProps,
+  "selectedValue" | "onValueChange"
+> {
+  label?: string;
+  value: PickerValue;
+  options: { label: string; value: PickerValue }[];
+  onChange: (value: PickerValue, index: number) => void;
+  required?: boolean;
+}
+const CustomPicker: React.FC<CustomPickerProps> = ({
+  label,
+  value,
+  options,
+  onChange,
+  required,
+  ...pickerProps
+}) => {
+  return (
+    <View
+      style={{
+        height: 40,
+        flexDirection: "row",
+        alignItems: "center",
+        backgroundColor: "#fff",
+        borderRadius: 12,
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 4,
+        elevation: 3,
+        overflow: "hidden",
+      }}
+    >
+      {label && (
+        <Text style={{ marginBottom: 6, fontWeight: "500" }}>{label}</Text>
+      )}
+
+      <Picker
+        selectedValue={value}
+        mode="dropdown"
+        onValueChange={onChange}
+        dropdownIconColor="#000"
+        style={{
+          flex: 1,
+          height: 40,
+        }}
+        {...pickerProps}
+      >
+        {options.map((op, index) => (
+          <Picker.Item
+            key={`${op.value ?? "null"}-${index}`}
+            label={op.label}
+            value={op.value}
+          />
+        ))}
+      </Picker>
+      {required && !value && (
+        <Text className="text-lg self-start text-red-500 p-1">*</Text>
+      )}
+    </View>
+  );
+};
+
+export default CustomPicker;
