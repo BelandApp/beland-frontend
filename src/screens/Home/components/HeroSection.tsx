fd64b2c48fa4dd534cd2e2b8347fe1ffd9cb6885
Victor De Menezes <estudiodemenezes@gmail.com>
2026-03-05 17:46:41 -0300

    fixing text error

--- src/screens/Home/components/HeroSection.tsx
+++ src/screens/Home/components/HeroSection.tsx
@@ -64,7 +64,7 @@ export const HeroSection: React.FC<HeroSectionProps> = ({
             onPress={() => navigate("MainTabs", { screen: "Wallet" })}
             style={styles.logoContainer}
           >
-            <Ionicons name="wallet-outline" size={20} color="#F97316" />s
+            <Ionicons name="wallet-outline" size={20} color="#F97316" />
             <Text style={styles.logoText}>Beland Wallet</Text>
           </TouchableOpacity>
         </View>
