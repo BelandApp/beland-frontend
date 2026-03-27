2e52171ae5e6d336b7934b457da3c0802d3326f8
Victor De Menezes <estudiodemenezes@gmail.com>
2026-03-27 13:24:45 -0300

    feat early return on error

--- src/screens/PayphoneSuccessScreen/hooks/usePayphoneConfirmation.ts
+++ src/screens/PayphoneSuccessScreen/hooks/usePayphoneConfirmation.ts
@@ -151,6 +151,7 @@ export function usePayphoneConfirmation() {
             setStatus(STATUS_MESSAGES.ALREADY_PROCESS);
             backendResult = null;
             setLoading(false);
+            return;
           }
         }
 
