a862c278577ebdedf9255c69836cf24f7bd49c42
Victor De Menezes <estudiodemenezes@gmail.com>
2026-05-11 14:32:28 +0200

    feat add modal for phone

--- src/screens/Login/LoginScreen.tsx
+++ src/screens/Login/LoginScreen.tsx
@@ -7,17 +7,26 @@ import {
   TouchableOpacity,
   KeyboardAvoidingView,
   Platform,
+  Image,
 } from "react-native";
-import { CustomInput, SocialButton, Button } from "@components/shared";
+import {
+  CustomInput,
+  SocialButton,
+  Button,
+  WrapperModal,
+  PhoneInput,
+} from "@components/shared";
 import { LoginWave } from "@components/ui";
 import { BelandLogo } from "@/components";
 import { styles } from "./styles";
-import { CircleArrowLeftIcon, Mail } from "lucide-react-native";
+import { CircleArrowLeftIcon, Mail, Phone } from "lucide-react-native";
 import { useLogin } from "./hook/useLogin";
 import { useAuth } from "src/context";
+import { colors } from "src/design-system";
+import { useResponsiveLayout } from "src/hooks";
+import { notify } from "src/hooks/notification/notify.external";
 export default function LoginScreen() {
-  const { width, height } = Dimensions.get("window");
-  const [showLocalLogin, setShowLocalLogin] = useState(false);
+  const { screenWidth, screenHeight } = useResponsiveLayout();
   const { user } = useAuth();
   const {
     handleLogin,
@@ -25,17 +34,36 @@ export default function LoginScreen() {
     setFormData,
     navigate,
     FormData,
-    isLoading,
+    status,
     errors,
+    showPhoneModal,
+    setShowPhoneModal,
+    showLocalLogin,
+    setShowLocalLogin,
+    phone,
+    handleAddPhone,
+    setPhone,
   } = useLogin();
   useEffect(() => {
-    if (!isLoading && user) {
+    if (status != "loading" && user?.phone) {
       navigate("MainTabs", {
         screen: "Home",
       });
     }
-  }, [user, isLoading]);
-
+    if (status === "authenticated" && !user?.phone) {
+      setShowPhoneModal(true);
+    }
+  }, [user, status]);
+  const handleBeforeClose = () => {
+    return new Promise<boolean>((resolve) => {
+      notify.confirm({
+        message:
+          "Esto es necesario para poder disfrutar de los servicios de Beland. ¿Cerrar igualmente?",
+        onConfirm: () => resolve(true),
+        onCancel: () => resolve(false),
+      });
+    });
+  };
   return (
     <KeyboardAvoidingView
       style={{ flex: 1 }}
@@ -53,13 +81,16 @@ export default function LoginScreen() {
           <CircleArrowLeftIcon size={32} color="#FFF" />
         </TouchableOpacity>
         <BelandLogo
-          width={width * 0.5}
-          height={height * 0.2}
+          width={screenWidth * 0.5}
+          height={screenHeight * 0.2}
           style={styles.logo}
         />
         <LoginWave />
         <View style={styles.container}>
-          <SocialButton onPress={handleLoginAuth0} disabled={isLoading} />
+          <SocialButton
+            onPress={handleLoginAuth0}
+            disabled={status === "loading"}
+          />
 
           <View
             style={{
@@ -106,7 +137,7 @@ export default function LoginScreen() {
                 title="Ingresar"
                 onPress={handleLogin}
                 variant="secondary"
-                isLoading={isLoading}
+                isLoading={status === "loading"}
               />
               <View style={styles.containerRow}>
                 <Text style={styles.subtitle}>¿Eres nuevo? </Text>
@@ -129,6 +160,42 @@ export default function LoginScreen() {
           )}
         </View>
       </ScrollView>
+      <WrapperModal
+        isOpen={showPhoneModal}
+        onClose={() => setShowPhoneModal(false)}
+        beforeClose={handleBeforeClose}
+        header={
+          <View className="flex-row items-center gap-2">
+            <Phone color={colors.brand.orange[500]} />
+            <Text className="font-semibold">
+              Termina de configurar tu cuenta
+            </Text>
+          </View>
+        }
+        content={
+          <View>
+            <PhoneInput
+              value={phone}
+              onChange={(value) => setPhone(value)}
+              required
+              error={errors.phone}
+            />
+            <Image
+              source={require("../../../assets/phoneRequired.png")}
+              className="rounded-3xl shadow mx-auto"
+              resizeMode="contain"
+              style={{
+                zIndex: 1,
+                width: screenWidth / 2,
+                height: screenHeight / 2,
+              }}
+            />
+          </View>
+        }
+        actions={
+          <Button title="Guardar" onPress={handleAddPhone} disabled={!phone} />
+        }
+      />
     </KeyboardAvoidingView>
   );
 }
