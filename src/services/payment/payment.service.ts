016724deceb2e1055443905df878f00a0ed8eb5a
Victor De Menezes <estudiodemenezes@gmail.com>
2026-05-14 22:21:14 +0200

    fixing errors

--- src/services/payment/payment.service.ts
+++ src/services/payment/payment.service.ts
@@ -18,17 +18,11 @@ export const PaymentStripeService = {
     if (!response.ok) throw new Error("Error creando payment intent on Stripe");
     return response;
   },
-<<<<<<< HEAD
-  createStripeUrlWeb: async (amount: number, userId: string) => {
-    const response = await core.post(`/stripe-topups/create-intent`, {
-      amountUsd: amount,
-=======
   createStripeUrlWeb: async (
     amountUsd: number,
   ): Promise<StripeIntentResponse> => {
     const response = await core.post(`/stripe-topups/create-intent`, {
       amountUsd,
->>>>>>> 7e3df05ff23c90ded290f2b1089340e4cf9045e0
     });
     return response;
   },
